582cbccc2a57131210ef17eca43182caba4d4807
Jonas <mz-martinez@live.com>
2022-05-01 15:40:26 -0300

    Inserindo DB

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'user.db',
+    autoload: true
+})
+
 module.exports = (app)=>{
 
     app.get('/users', (req, res) => {
@@ -14,8 +20,17 @@ module.exports = (app)=>{
     });
     
     app.post('/users', (req, res)=>{
-    
-        res.json(req.body);
+
+        db.insert(req.body, (err, user)=>{
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            }else{
+                res.status(200).json(user);
+            }
+        })
     
     })
 
